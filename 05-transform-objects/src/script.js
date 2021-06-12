7ba2647dbfa7fa47537909b807609b008329218a
Calil <amaral.calil@gmail.com>
2021-06-12 19:35:24 -0300

    feature: add axes helper and change positions

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -13,23 +13,29 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 /**
  * Set mesh position
  */
-// mesh.position.x = 3
-// mesh.position.y = 7
-// mesh.position.z = 5
-mesh.position.set(3,7,5);
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+mesh.position.set(0.7,-0.6,1);
 console.log(mesh.position.length())
 
 /**
  * Update and calculate properties
  */
-mesh.position.normalize()
-console.log(mesh.position.length())
+// mesh.position.normalize()
+// console.log(mesh.position.length())
 
-scene.add(mesh)
+
+/**
+ * Add axes helper
+ */
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -43,7 +49,7 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.set(1,1,3)
 scene.add(camera)
 
 /**
