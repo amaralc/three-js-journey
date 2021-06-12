f731398b9c0d2263807052416fdeecd8346cedb3
Calil <amaral.calil@gmail.com>
2021-06-12 19:23:26 -0300

    feature: change cube position

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -13,6 +13,9 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.x = 3
+mesh.position.y = 7
+mesh.position.z = 5
 scene.add(mesh)
 
 /**
