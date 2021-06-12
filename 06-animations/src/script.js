267d693a82eeabf01980c98d2e9b0e504f8d7c8c
Calil <amaral.calil@gmail.com>
2021-06-12 20:40:46 -0300

    feature: change date.now by three.clock

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -31,20 +31,19 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
-// Time
-let time = Date.now()
+// Clock
+const clock = new THREE.Clock()
+
 
 // Animations
 const tick = () => {
 
-  // Time
-  const currentTime = Date.now()
-  const deltaTime = currentTime - time
-  time = currentTime
-
+  // Clock
+  const elapsedTime = clock.getElapsedTime()
+  console.log(elapsedTime)
 
   // Update objects
-  mesh.rotation.y += 0.001 * deltaTime
+  mesh.rotation.y = elapsedTime
 
   // Render
   renderer.render(scene, camera)
