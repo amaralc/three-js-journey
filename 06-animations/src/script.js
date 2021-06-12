8762e73f3d52220307e19b6b6d23f37a947495b1
Calil <amaral.calil@gmail.com>
2021-06-12 20:37:34 -0300

    feature: normalize speed using time

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -31,11 +31,20 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
+// Time
+let time = Date.now()
 
 // Animations
 const tick = () => {
+
+  // Time
+  const currentTime = Date.now()
+  const deltaTime = currentTime - time
+  time = currentTime
+
+
   // Update objects
-  mesh.rotation.y += 0.01
+  mesh.rotation.y += 0.001 * deltaTime
 
   // Render
   renderer.render(scene, camera)
