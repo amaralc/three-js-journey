6b1ee460c8c26d2421725eca8125d4861f857f26
Calil <amaral.calil@gmail.com>
2021-06-13 13:56:38 -0300

    feature: add orthographic camera

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -24,7 +24,8 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100)
 camera.position.x = 2
 camera.position.y = 2
 camera.position.z = 2
@@ -55,3 +56,12 @@ const tick = () =>
 }
 
 tick()
+
+/** 
+ * References
+ * 
+ * https://threejs-journey.xyz/lessons/7
+ * https://threejs.org/docs/index.html?q=camera#api/en/cameras/PerspectiveCamera
+ * https://threejs.org/docs/index.html?q=camera#api/en/cameras/OrthographicCamera
+ * 
+ */
